d68432cef9e7601882a9c58332b2452388f6bcc4
fydrenak <fydrenak@gmail.com>
2018-04-14 15:54:40 +0900

    Created the ascital project, swapped the fratal project on the main page and restyled the blog post list.

---
+++ projects/ascital/ascital.js
@@ -0,0 +1,47 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+//(a + bi)(a + bi)
+//a^2 + 2abi - b^2
+//re = a^2 - b^2
+//im = 2ab
+
+function mapCoords(value, oLow, oMax, nLow, nMax) {
+    return nLow + ((value - oLow) / (oMax - oLow)) * (nMax - nLow)
+}
+
+
+function renderFractal(maxIter, width, height, xMin = -2, xMax = 1, yMin = 1, yMax = -1) {
+    const area = document.getElementById("fractal")
+
+    for (let y = 0; y < height; ++y) {
+        let line = document.createElement("p")
+
+        for (let x = 0; x < width; ++x) {
+            const cre = mapCoords(x, 0, width - 1, xMin, xMax)
+            const cim = mapCoords(y, 0, height - 1, yMin, yMax)
+            let zre = 0, zim = 0
+            let iter = 0
+
+            while (iter < maxIter && zre*zre + zim*zim < 2) {
+                //compute z = z^2 + c
+                const newzre = zre*zre - zim*zim
+                const newzim = 2 * zre * zim
+
+                zre = newzre + cre
+                zim = newzim + cim
+
+                ++iter
+            }
+
+            let char = ' '
+
+            if (iter < maxIter) {
+                char = alphabet[iter % alphabet.length]
+            }
+
+            line.innerText += char
+        }
+
+        area.appendChild(line)  
+    }
+}
