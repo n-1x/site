52c50a53638b90d863531bff63243454f7630b09
n-1x <0@00.lu>
2023-11-27 15:24:47 +0000

    Implement WebGL best practices
    - No longer checking compile status of shaders. Now only checking
    link status and attempt to use KHR non-blocking checks.
    - Minor formatting changes.

--- resources/js/background.js
+++ resources/js/background.js
@@ -7,46 +7,19 @@ let uniformTime = null;
 let uniformMouse = null;
 let uniformHSL = null;
 let root = document.querySelector(":root");
+let shaderProgram;
 
-window.addEventListener("DOMContentLoaded", () => {
-    canvas = document.createElement("canvas");
-    canvas.classList.add("bgcanv");
-    canvas.width = window.innerWidth;
-    canvas.height = document.documentElement.scrollHeight;
-    document.body.appendChild(canvas);
+/* Step3: Create and compile Shader programs */
+// Vertex shader source code
+const vertCode = `
+    attribute vec2 coordinates;
+    void main(void) {
+    gl_Position = vec4(coordinates,0.0, 1.0);
+    }
+`;
 
-    gl = canvas.getContext('webgl');
-
-    // Create a new buffer object
-    const vertex_buffer = gl.createBuffer();
-    const index_buffer = gl.createBuffer();
-
-    // Bind an empty array buffer to it
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-
-    // Pass the vertices data to the buffer
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
-    // Unbind the buffers
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    /* Step3: Create and compile Shader programs */
-    // Vertex shader source code
-    const vertCode = `
-        attribute vec2 coordinates;
-        void main(void) {
-        gl_Position = vec4(coordinates,0.0, 1.0);
-        }
-    `
-    const vertShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertShader, vertCode);
-    gl.compileShader(vertShader);
-
-    //Fragment shader source code
-    const fragCode = `
+//Fragment shader source code
+const fragCode = `
         precision mediump float;
         
         uniform vec2 iResolution;
@@ -93,25 +66,74 @@ window.addEventListener("DOMContentLoaded", () => {
 
         gl_FragColor = vec4(col, 1.);
         }
-    `
-            
-    // Create fragment shader object
-    const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragShader, fragCode);
-    gl.compileShader(fragShader);
+    `;
 
-    //check frag shader for compilation errors
-    if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
-        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(fragShader));
-        gl.deleteShader(fragShader);
+function checkLinkStatusAndDraw(vs, fs) {
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.error(`Link failed: ${gl.getProgramInfoLog(shaderProgram)}`);
+        console.error(`vs info-log: ${gl.getShaderInfoLog(vs)}`);
+        console.error(`fs info-log: ${gl.getShaderInfoLog(fs)}`);
     }
+    else {
+        renderInit();
+    }
+}
 
-    // Create a shader program object to store combined shader program
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertShader);
-    gl.attachShader(shaderProgram, fragShader);
+function startWebGL() {
+    gl = canvas.getContext('webgl');
+    shaderProgram = gl.createProgram()
+
+    const ext = gl.getExtension("KHR_parallel_shader_compile");
+    const vs = gl.createShader(gl.VERTEX_SHADER);
+    const fs = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vs, vertCode);
+    gl.shaderSource(fs, fragCode);
+
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+
+    gl.attachShader(shaderProgram, vs);
+    gl.attachShader(shaderProgram, fs);
+
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
+
+    if (ext) {
+        if (gl.getProgramParameter(shaderProgram, ext.COMPLETION_STATUS_KHR)) {
+            checkLinkStatusAndDraw(vs, fs);
+        }
+    }
+    else {
+        checkLinkStatusAndDraw(vs, fs);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    canvas = document.createElement("canvas");
+    canvas.classList.add("bgcanv");
+    canvas.width = window.innerWidth;
+    canvas.height = document.documentElement.scrollHeight;
+    document.body.appendChild(canvas);
+    startWebGL();
+});
+
+function renderInit() {
+    // Create a new buffer object
+    const vertex_buffer = gl.createBuffer();
+    const index_buffer = gl.createBuffer();
+
+    // Bind an empty array buffer to it
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+
+    // Pass the vertices data to the buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    // Unbind the buffers
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     const uniformResolution = gl.getUniformLocation(shaderProgram, "iResolution");
     gl.uniform2f(uniformResolution, window.innerWidth, document.documentElement.scrollHeight);
@@ -123,7 +145,7 @@ window.addEventListener("DOMContentLoaded", () => {
     gl.uniform2f(uniformMouse, 0.0, 0.0);
 
     uniformHSL = gl.getUniformLocation(shaderProgram, "colour");
-    const [r,g,b] = getAccentRGB();
+    const [r, g, b] = getAccentRGB();
     gl.uniform3f(uniformHSL, r, g, b);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
@@ -135,36 +157,35 @@ window.addEventListener("DOMContentLoaded", () => {
 
     gl.clearColor(0.5, 0.5, 0.5, 0.9);
 
-    update();
-
     document.onmousemove = event => {
-        gl.uniform2f(uniformMouse, 
-        event.clientX, 
-        document.documentElement.scrollHeight - event.clientY
+        gl.uniform2f(uniformMouse,
+            event.clientX,
+            document.documentElement.scrollHeight - event.clientY
         );
     };
-    
+
     window.onresize = () => {
         canvas.width = window.innerWidth;
         canvas.height = document.documentElement.scrollHeight;
         gl.uniform2f(uniformResolution, canvas.width, canvas.height);
     };
-});
+
+    render();
+}
 
 // TODO: touch support (events: touchstart, touchmove)
-
-function update() {
+function render() {
     const t = performance.now() / 1000.;
     gl.uniform1f(uniformTime, t);
 
     setAccent((Date.now() / 150) % 360);
-    const [r,g,b] = getAccentRGB();
+    const [r, g, b] = getAccentRGB();
     gl.uniform3f(uniformHSL, r, g, b);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-    window.requestAnimationFrame(update);
+    window.requestAnimationFrame(render);
 }
 
 function hueToRGB(m1, m2, h) {
@@ -184,7 +205,7 @@ function hueToRGB(m1, m2, h) {
         result = m2;
     }
     else if (h * 3. < 2.) {
-        result = m1 + (m2 - m1) * (2./3.- h) * 6.;
+        result = m1 + (m2 - m1) * (2. / 3. - h) * 6.;
     }
 
     return result;
@@ -192,7 +213,7 @@ function hueToRGB(m1, m2, h) {
 
 function hslToRGB(h, s, l) {
     let m1, m2;
-    
+
     if (l <= 0.5) {
         m2 = l * (s + 1.);
     }
@@ -202,11 +223,11 @@ function hslToRGB(h, s, l) {
 
     m1 = l * 2. - m2;
 
-    const r = hueToRGB(m1, m2, h + 1./3.);
-    const g = hueToRGB(m1, m2, h        );
-    const b = hueToRGB(m1, m2, h - 1./3.);
+    const r = hueToRGB(m1, m2, h + 1. / 3.);
+    const g = hueToRGB(m1, m2, h);
+    const b = hueToRGB(m1, m2, h - 1. / 3.);
 
-    return [r,g,b];
+    return [r, g, b];
 }
 
 function getAccentRGB() {
@@ -214,7 +235,7 @@ function getAccentRGB() {
     const hue = style.getPropertyValue("--accent-hue");
     const sat = parseInt(style.getPropertyValue("--accent-sat"));
 
-    return hslToRGB(hue/360., sat/100., 0.5);
+    return hslToRGB(hue / 360., sat / 100., 0.5);
 }
 
 function setAccent(hue) {
